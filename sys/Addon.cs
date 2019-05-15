2ba5c36ac3daf0de7559b235b72a780a6bc16f14
CrashGamesCrMc <crash.games.cr.mc.yt@gmail.com>
2019-05-15 20:05:03 +0200

    0.0.1-not runnable

---
+++ sys/Addon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addon
+{
+    public class Loader
+    {
+        public string[] GetFunctions() => new string[]{"test"};
+    }
+    public class Functions
+    {
+        public string test(string cmd)
+        {
+            Console.WriteLine("sys lib running test-function; called by line: \"" + cmd + "\"");
+            return "success!";
+        }
+    }
+}
--- uapl/Interpeter.cs
+++ uapl/Interpeter.cs
@@ -10,12 +10,67 @@ namespace uapl
     
     public class Interpeter
     {
-        public List<String> variable_names = new List<string>();
+        public List<string> variable_names = new List<string>();
         public List<dynamic> variables = new List<dynamic>();
 
-        public List<String> function_names = new List<string>();
+        public List<string> function_names = new List<string>();
         public List<bool> function_types = new List<bool>(); // is it native?
-        public List<dynamic> function = new List<dynamic>(); // where is the function located in text?
+        public List<dynamic> functions = new List<dynamic>(); // where is the function located in text?
+        public List<int> native_instance_indexes = new List<int>();
+        public List<dynamic> native_instances = new List<dynamic>();
+
+        public bool AddFunction(string name, string[] code, bool _override)
+        {
+            if (function_names.IndexOf(name) == -1)
+            {
+                function_names.Add(name);
+                function_types.Add(false);
+                functions.Add(code);
+                native_instance_indexes.Add(-1);
+                return true;
+            }
+            else
+            {
+                if (_override)
+                {
+                    int index = function_names.IndexOf(name);
+                    function_types[index] = false;
+                    functions[index] = code;
+                    native_instance_indexes.Add(-1);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public bool AddNativeFunction(string name, MethodInfo code, int native_instance_index, bool _override)
+        {
+            if (function_names.IndexOf(name) == -1)
+            {
+                function_names.Add(name);
+                function_types.Add(true);
+                functions.Add(code);
+                native_instance_indexes.Add(native_instance_index);
+                return true;
+            }
+            else
+            {
+                if (_override)
+                {
+                    int index = function_names.IndexOf(name);
+                    function_types[index] = true;
+                    functions[index] = code;
+                    native_instance_indexes.Add(native_instance_index);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
 
         public String RunStatement(String statement)
         {
@@ -30,12 +85,31 @@ namespace uapl
                     MethodInfo method = type.GetMethod("GetFunctions");
                     string[] result = method.Invoke(loader_instance, new object[] { });
 
-                    foreach ()
+                    Type function_type = dll.GetType("Addon.Functions");
+                    dynamic function_instance = Activator.CreateInstance(function_type);
 
+                    int index = native_instances.Count;
+                    native_instances.Add(function_instance);
+
+                    foreach (string function in result)
+                    {
+                        AddNativeFunction(function, function_type.GetMethod(function), index, false);
+                    }
                 }
                 else
                 {
-
+                    int index = function_names.IndexOf(split[1]);
+                    if (index != -1)
+                    {
+                        if (function_types[index])
+                        {
+                            return functions[index].Invoke(native_instances[native_instance_indexes[index]], new object[] { statement });
+                        }
+                        else
+                        {
+                            throw new NotImplementedException("UAPL non-built-in functions not implemented yet!");
+                        }
+                    }
                 }
             }
             return null;
