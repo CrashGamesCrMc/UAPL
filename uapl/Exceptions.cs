f38700ed46021cb9d6ddf6e6e338254fb1063d99
CrashGamesCrMc <crash.games.cr.mc.yt@gmail.com>
2019-05-15 19:19:48 +0200

    test msg

---
+++ uapl/Exceptions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uapl
+{
+    public class Exceptions
+    {
+        public class SyntaxException : Exception
+        {
+            public SyntaxException(string message) : base(message)
+            {
+            }
+        }
+        public class VariableExceptions
+        {
+            public class VariableException : Exception
+            {
+                public VariableException(string message) : base(message)
+                {
+                }
+            }
+            public class VariableDoesNotExistException : VariableException
+            {
+                public VariableDoesNotExistException(string message) : base(message)
+                {
+                }
+            }
+        }
+        public class FunctionExcepetions
+        {
+            public class FunctionException : Exception
+            {
+                public FunctionException(string message) : base(message)
+                {
+                }
+            }
+            public class FunctionDoesNotExistException : FunctionException
+            {
+                public FunctionDoesNotExistException(string message) : base(message)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ uapl/Interpeter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uapl
+{
+    
+    public class Interpeter
+    {
+        public List<String> variable_names = new List<string>();
+        public List<dynamic> variables = new List<dynamic>();
+
+        public List<String> function_names = new List<string>();
+        public List<bool> function_types = new List<bool>(); // is it native?
+        public List<dynamic> function = new List<dynamic>(); // where is the function located in text?
+
+        public String RunStatement(String statement)
+        {
+            String[] split = statement.Split(' ');
+            if (split[0] == "run")
+            {
+                if (split[1] == "import")
+                {
+                    Assembly dll = Assembly.LoadFile(split[2]);
+                    Type type = dll.GetType("Addon.Loader");
+                    dynamic loader_instance = Activator.CreateInstance(type);
+                    MethodInfo method = type.GetMethod("GetFunctions");
+                    string[] result = method.Invoke(loader_instance, new object[] { });
+
+                    foreach ()
+
+                }
+                else
+                {
+
+                }
+            }
+            return null;
+        }
+    }
+}
